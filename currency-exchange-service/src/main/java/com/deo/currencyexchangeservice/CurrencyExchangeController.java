b99ad49036e99654c71ac25c130f52ab725c5c7d
Serhii Lukianenko <lukyanenko88@gmail.com>
2021-08-16 10:47:08 +0300

    add simple logger to CurrencyExchangeController for testing

--- currency-exchange-service/src/main/java/com/deo/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/deo/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.deo.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+
+   private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     CurrencyExchangeRepo currencyExchangeRepo;
     Environment environment;
 
@@ -19,6 +24,8 @@ public class CurrencyExchangeController {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+
+        logger.info("retrieveExchangeValue called with parameters : {}: {}",from,to);
 
         CurrencyExchange currencyExchange = currencyExchangeRepo.findByFromAndTo(from, to);
         if (currencyExchange == null) {
